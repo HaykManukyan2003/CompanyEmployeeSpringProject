dfe0b0fed4633d136b2494609ad775d904fbf583
Hayk <haykmanukyan2003@gmail.com>
2022-09-28 11:50:55 +0400

    Initial commit

---
+++ src/main/java/am/itspace/companyemployeespringproject/CompanyEmployeeSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package am.itspace.companyemployeespringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyEmployeeSpringProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompanyEmployeeSpringProjectApplication.class, args);
+    }
+
+}
